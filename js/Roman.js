e1ed805ca55fe33193535d9b755016b5b8639e83
sven erik rebane <sven erik rebane>
2017-06-14 16:38:16 +0300

    readme.md update and almost working check system

--- js/Roman.js
+++ js/Roman.js
@@ -1,78 +1,257 @@
-var roman = 'IVXLCDM';
-
-
 function RA() {
   console.time();
   var  RAvalue = document.getElementById('RAinput').value;
+  var RAvalue =  RAvalue.toUpperCase();
   console.group('Roman to arabic, string ('+RAvalue+')');
-  var func = check(RAvalue);
-  console.log(func);
-  var result = func.result;
-  var RAvalue = func.RAvalue;
-  var type = func.type;
+  var check = RAhub(RAvalue);
+  //type: valid.type, occurence: valid.occurence, letter1: valid.letter1, letter2: valid.letter2, valid: valid.valid
 
   var p = document.createElement('p1');
   var br = document.createElement('br');
   var log = document.getElementById("log");
-
-  if (type == "answer"){
-    var text = document.createTextNode("Answer to your roman number "+RAvalue+" is "+result);
-
-    p.appendChild(text);
-    p.className = type;
-    log.insertBefore(p, log.childNodes[0]);
-    log.insertBefore(br, log.childNodes[1]);
-
-  }
-  else if (type == 'error') {
-    var text = document.createTextNode("Your roman number ("+RAvalue+") is incorrect and can't be converted");
+  if (check.valid == "true"){
+    var text = document.createTextNode("Answer to your roman number "+check.RAvalue+" is "+check.result);
 
     p.appendChild(text);
-    p.className = type;
-    log.insertBefore(p, log.childNodes[0]);
-    dlog.insertBefore(br, log.childNodes[1]);
-  }
-  else {
-    console.error('log txt error, type = '+type+', result = '+result+', RAvalue = '+RAvalue);
-
-    var text = document.createTextNode("Your converted number message was not able to send");
-
-    p.appendChild(text);
-    p.className = "error";
-    log.insertBefore(p, log.childNodes[0]);
-    log.insertBefore(br, log.childNodes[1]);
-  }
-}
-
-function check(RAvalue){
-
-  if (isNaN(RAvalue)) {
-    console.log('string ('+RAvalue+') is valid');
-    var result = convert(RAvalue);
-    return { result: result, type: "answer", RAvalue: RAvalue};
-  }
-
-  else {
-    console.error("Your input is a number");
-
-    var p = document.createElement('p1');
-    var br = document.createElement('br');
-    var text = document.createTextNode("Your input is a number");
-    var log = document.getElementById("log")
-
-    p.appendChild(text);
-    p.className = "error";
+    p.className = check.type;
     log.insertBefore(p, log.childNodes[0]);
     log.insertBefore(br, log.childNodes[1]);
 
     console.timeEnd();
     console.groupEnd();
+
+  }
+  else if (check.valid == 'false') {
+    if (check.type == "LError"){
+      var text = document.createTextNode("Number "+check.letter+" does not exist in roman numbers");
+
+      p.appendChild(text);
+      p.className = check.type;
+      log.insertBefore(p, log.childNodes[0]);
+      log.insertBefore(br, log.childNodes[1]);
+
+      console.timeEnd();
+      console.groupEnd();
+    }
+    if (check.type == "NError"){
+      var text = document.createTextNode("Answer to your roman number "+check.RAvalue+" is "+check.result);
+
+      p.appendChild(text);
+      p.className = check.type;
+      log.insertBefore(p, log.childNodes[0]);
+      log.insertBefore(br, log.childNodes[1]);
+
+      console.timeEnd();
+      console.groupEnd();
+    }
+    if (check.type == "CError1" ){
+      var text = document.createTextNode("You can't have number " +check.letter+ ", " +check.occurence+ " times.");
+
+      p.appendChild(text);
+      p.className = check.type;
+      log.insertBefore(p, log.childNodes[0]);
+      log.insertBefore(br, log.childNodes[1]);
+
+      console.timeEnd();
+      console.groupEnd();
+    }
+    if (check.type == "CError2" ){
+      var text = document.createTextNode("You can't have number " +check.letter1+ " and number " +check.letter2+ " at the same time.");
+
+      p.appendChild(text);
+      p.className = check.type;
+      log.insertBefore(p, log.childNodes[0]);
+      log.insertBefore(br, log.childNodes[1]);
+
+      console.timeEnd();
+      console.groupEnd();
+    }
+    if (check.type == "CError3" ){
+      var text = document.createTextNode("You can't have number " +check.letter1+ " over one time, when number " +check.letter2+ " exists.");
+
+      p.appendChild(text);
+      p.className = check.type;
+      log.insertBefore(p, log.childNodes[0]);
+      log.insertBefore(br, log.childNodes[1]);
+
+      console.timeEnd();
+      console.groupEnd();
+    }
   }
 }
 
-function convert(RAvalue){
+function RAhub(RAvalue){
+  if (isNaN(RAvalue)) {
+    var valid = RAcheck(RAvalue);
+    if (valid.valid == "true"){
+      var result = RAconvert(RAvalue);
+      if (result.valid == "true"){
+        var pos = RApos(result.numberArray);
+        if (pos.valid == "true") {
+          return { result: result.result, valid: pos.valid, RAvalue: RAvalue};
+        }
+        else if (pos.valid == "false") {
+          return { type: pos.type, valid: pos.valid}
+        }
+      }
+      else if (result.valid == "false") {
+        return { type: result.type, letter: result.letter , valid: result.valid}
+      }
+    }
+    else if (valid.valid == "false") {
+      return {type: valid.type, occurence: valid.occurence, letter: valid.letter, letter1: valid.letter1, letter2: valid.letter2, valid: valid.valid}
+    }
+  }
+  else {
+    return {type: "NError", valid: "false"}
+  }
+}
+
+function RAcheck(RAvalue) {
+  var unique = ["IV", "IX", "XL", "XC", "CD", "CM"];
+  var validNum = ["I", "V", "X", "L", "C", "D", "M"];
+  if (RAvalue.match(/validNum/g)){
+    return { type: "LError", letter: RAvalue.charAt(i), valid: "false"}
+  }
+  if (RAvalue.match(/IV/g) != "null"){
+    var i = (RAvalue.match(/IV/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "IV", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/I/g) || []).length;
+      if (i == 1){
+        if (RAvalue.match(/IX/g) == "null"){
+          unique.delete[0];
+        }
+        else if (RAvalue.match(/IX/g) == "null"){
+          return { type: "CError2", letter1: "IV", letter2: "IX", valid: "false"}
+        }
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "I", letter2: "IV", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/IX/g) != "null"){
+    var i = (RAvalue.match(/IX/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "IX", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/I/g) || []).length;
+      if (i == 1){
+        unique.delete[1];
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "I", letter2: "IX", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/XL/g) != "null"){
+    var i = (RAvalue.match(/XL/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "XL", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/X/g) || []).length;
+      if (i == 1){
+        if (RAvalue.match(/XC/g) == "null"){
+          unique.delete[2];
+        }
+        else if (RAvalue.match(/XC/g) = "null"){
+          return { type: "CError2", letter1: "IXLV", letter2: "XC", valid: "false"}
+        }
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "X", letter2: "XL", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/XC/g) != "null"){
+    var i = (RAvalue.match(/XC/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "XC", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/X/g) || []).length;
+      if (i == 1){
+        unique.delete[1];
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "X", letter2: "XC", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/CD/g) != "null"){
+    var i = (RAvalue.match(/CD/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "CD", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/C/g) || []).length;
+      if (i == 1){
+        if (RAvalue.match(/CM/g) == "null"){
+          unique.delete[2];
+        }
+        else if (RAvalue.match(/CM/g) == "null"){
+          return { type: "CError2", letter1: "IXLV", letter2: "CM", valid: "false"}
+        }
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "C", letter2: "CD", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/XC/g) != "null"){
+    var i = (RAvalue.match(/CM/g) || []).length;
+    if (i > 1) {
+      return { type: "CError1", letter: "CM", occurence: i, valid: "false"}
+    }
+    else if (i == 1) {
+      var i = (RAvalue.match(/C/g) || []).length;
+      if (i == 1){
+        unique.delete[1];
+      }
+      else if (i > 1) {
+        return { type: "CError3", letter1: "C", letter2: "CM", valid: "false"}
+      }
+    }
+  }
+  if (RAvalue.match(/M/g) != "null"){
+    var i = (RAvalue.match(/M/g) || []).length;
+    if (i > 3){
+      console.log("M");
+      return { type: "CError1", letter: "M", occurence: i, valid: "false"}
+    }
+  }
+
+  if (unique.includes("IV") && unique.includes("IX")){
+    var i = (RAvalue.match(/I/g) || []).length;
+    if (i > 3){
+      return { type: "CError1", letter: "I", occurence: i, valid: "false"}
+    }
+  }
+  if (unique.includes("XL") && unique.includes("XC")){
+    var i = (RAvalue.match(/X/g) || []).length;
+    if (i > 3){
+      return { type: "CError1", letter: "X", occurence: i, valid: "false"}
+    }
+  }
+  if (unique.includes("CD") && unique.includes("CM")){
+    var i = (RAvalue.match(/C/g) || []).length;
+    if (i > 3){
+      return { type: "CError1", letter: "C", occurence: i, valid: "false"}
+    }
+  }
+  return {valid: "true"};
+}
+
+function RAconvert(RAvalue){
   RAvalue = RAvalue.toUpperCase();
+  var roman = 'IVXLCDM';
   var numbers = [];
+
 
   for (var i = 0; i <= RAvalue.length; i++){
     // I = 1, II = 2, III = 3, IV = 4, IX = 9
@@ -83,7 +262,7 @@ function convert(RAvalue){
           RAvalue = RAvalue.replace('I', '0');
           RAvalue = RAvalue.replace('I', '0');
           numbers.push(3);
-          i = i + 2
+          i = i + 2;
         }
         else {
           RAvalue = RAvalue.replace('I', '0');
@@ -151,7 +330,7 @@ function convert(RAvalue){
           RAvalue = RAvalue.replace('C', '0');
           RAvalue = RAvalue.replace('C', '0');
           numbers.push(300);
-          i = i + 2
+          i = i + 2;
         }
         else {
           RAvalue.replace('C', '0');
@@ -190,7 +369,7 @@ function convert(RAvalue){
         else {
           RAvalue = RAvalue.replace('M', '0');
           RAvalue = RAvalue.replace('M', '0');
-          numbers.push(2000)
+          numbers.push(2000);
           i = i++;
         }
       }
@@ -214,11 +393,15 @@ function convert(RAvalue){
       RAvalue = RAvalue.replace('D', '0');
       numbers.push(500);
     }
+  return { result: numbers.reduce( function(prev, cur) { return prev + cur; } ), numberArray: numbers, valid: "true" }
   }
+}
 
-    console.log('array is '+numbers);
-    console.log('answer is '+RAvalue);
-    console.timeEnd();
-    console.groupEnd();
-    return numbers.reduce( function(prev, cur) { return prev + cur; } );
+function RApos(numbers) {
+  for (i = 0; i > numbers.length; i++) {
+    if (numbers[i] < numbers[i+1]) {
+      return {type: "PosError", valid: "false"};
+    }
+  }
+    return {valid: "true"};
 }
--- readme.md
+++ readme.md
@@ -11,32 +11,70 @@ This is a number converter, it convers diffrent types of numbers to arabic.
 
 #### Roman to Arabian
 
-* ```function RA() ```
-  * Starts console.time
-  * Takes string from ```class="RAinput"```
-  * Writes it to ```var RAvalue;```
-  * Start console.group
-  * Starts ```function check()```
+###### Variables
 
-<br />
+| Name | Location | Use |
+| :---: | :---: | :--- |
+| RAvalue | RA() | This is HTML input |
+| validNum | RAhub | Checks if all inputs characters are valid |
+| roman | RAconvert | Contains string which is used to find match to input |
+| unique | RAcheck | Used to make sure that strings dosen't contain invalid roman numbers |
+| numbers | RAconvert | Pushes the corrosponding number match from string to array and sums it |
+| type | returns | If valid is "false" this says the error |
+| occurence | returns | Used to count how many times a invalid number was met |
+| valid | returns | This says if input is "true"/"false" |
+| letter | returns  | This is invalid letter |
+| letter1 | returns | This is invalid letter |
+| letter2 | returns | This is invalid letter |
 
-* ```function check()```
-  * If string is not a number
-    * Starts ```function convert()```
-    * Logs that string is valid
-  * Else
-    * Logs that string is invalid
-    * Starts ```function end()```...
+<br>
 
-<br />
+###### Function variables
 
-* ```function convert()```
-  * While loop untill ```i``` is bigger then ```RAvalue.length```
-    * If character at ```i``` is character {I, V, X, L, C, D, M}
-      * Pushes the corresponding number to an array ```var numbers = [];```
-      * Replaces the character in string ```RAvalue```with 0
-      * I, X, C, M have other number combiations aswell so depenging if that combination takes 2 letters or 3, the amount will be replaced in string ```RAvalue``` and the corrosponding numbers will be sent to the array
+| Name | From function |
+| :---: | :---: |
+| check | RAhub |
+| valid | RAcheck |
+| result | RAconvert |
+| pos | RApos |
 
-<br />
+<br>
 
-*
+###### Errors
+| Type | When occures | HTML message
+| :---: | :--- | :--- |
+| LError | When it meets a invalid letter | Number <letter> does not exist in roman numbers |
+| NError | When input string contains numbers |
+| CError1 | When too many of characters exists | You can't have number <letter>, <occurence> times |
+| CError2 | When 2 numbers exists which can't exist at the same time | You can't have number <letter1> and number <letter2> at the same time. |
+| CError3 | When number exists after which another number can't exists over 1 time |You can't have number <letter1> over one time, when number <letter2> exists. |
+
+<br>
+
+* ```javascript function RA() ```
+  * This is start function
+  * Actions
+    * Prints/Takes info to/from HTML
+    * makes input upper casse
+    * Starts console.time
+    * Starts ```javascript function RAhub(RAvalue) ```
+
+<br>
+
+* ```javascript function RAhub(RAvalue) ```
+  * This is center of functions
+  * Actions
+    * checks for numbers
+      * If no numbers are met it continues
+      * If number is met it returns
+    * starts  ```javascript function RAcheck(RAvalue) ```
+      * Checks that there aren't too many roman umbers in case
+      * If function returns "valid" it continues
+      * IF function returns "false" it returns, type: CError
+    * starts  ```javascript function RAconvert(RAvalue) ```
+      * It converts the roman numbers to arabic numbers and sums it up
+      * This function will return the converted number
+    * starts  ```javascript function RApos(result.numberArray) ```
+      * Checks that numbers are in right order
+      * If function returns "valid" it returns
+      * IF function returns "false" it returns, type: PosError
